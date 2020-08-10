10ffbb5ea872c7c3ce58358f15542ec175965aaf
cd <cdominik@gmail.com>
2020-08-10 23:37:49 +0200

    Updates

---
+++ precache-manifest.d441118d291330280bd252ba1594497a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81269c9fd1b99d8cd0de62892029885a",
+    "url": "/chase-your-dreams/index.html"
+  },
+  {
+    "revision": "b57faeda1a02623859a2",
+    "url": "/chase-your-dreams/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "dd3b8062afbaf7d07f85",
+    "url": "/chase-your-dreams/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b57faeda1a02623859a2",
+    "url": "/chase-your-dreams/static/js/2.dfe5b86a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/chase-your-dreams/static/js/2.dfe5b86a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3b8062afbaf7d07f85",
+    "url": "/chase-your-dreams/static/js/main.04ff1cb4.chunk.js"
+  },
+  {
+    "revision": "a4d64f9b5e2b4d86dfc8",
+    "url": "/chase-your-dreams/static/js/runtime-main.f5ce10c1.js"
+  }
+]);
